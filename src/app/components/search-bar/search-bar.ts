4dd8070817b76f7787a9dba7114e3d313433e575
miguel <miguel.arellano@jaak.ai>
2026-02-04 20:32:41 -0600

    feat: the search for name function was implemented in search component

--- src/app/components/search-bar/search-bar.ts
+++ src/app/components/search-bar/search-bar.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { CharacterService } from '../../services/character/character-service';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
@@ -14,5 +16,16 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './search-bar.scss',
 })
 export class SearchBar {
+  private searchTimeout: any;
+  constructor(private characterSrv: CharacterService) { }
 
+  public searchCharacter = async (name: string):Promise <void> => {
+    if (this.searchTimeout) {
+      clearTimeout(this.searchTimeout);
+    }
+    this.searchTimeout = setTimeout(async () => {
+      if(name.trim().length > 0) this.characterSrv.character.set(await firstValueFrom(this.characterSrv.getCharacterForName(name)));
+      else this.characterSrv.character.set(await firstValueFrom(this.characterSrv.getCharacter()));
+    }, 500);
+  }
 }
--- src/app/services/character/character-service.ts
+++ src/app/services/character/character-service.ts
@@ -1,24 +1,31 @@
-import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { Injectable, signal } from '@angular/core';
+import { async, map, Observable } from 'rxjs';
 import { Character } from '../../classes/Character';
 import { HttpClient } from '@angular/common/http';
 import { BuilderService } from '../builder/builder';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class CharacterService {
+  public character = signal<Character>(null);
 
   constructor(
     private http: HttpClient,
     private builderSrv: BuilderService
   ) { }
 
-  public character = ():Observable<Character> => {
+  public getCharacter = ():Observable<Character> => {
     return this.http.get<Character>('https://rickandmortyapi.com/api/character').pipe(
       map((response: any) => this.builderSrv.bulderCharacterclass(response))
     )
-  }
+  } 
   
+  public getCharacterForName = (name: string): Observable<Character> => {
+    return this.http.get<Character>(`https://rickandmortyapi.com/api/character?name=${name}`).pipe(
+      map((response: any) => this.builderSrv.bulderCharacterclass(response))
+    )
+  }
 }
 
